bd860dedd1036c92561139dc18bdc76e261be827
timdog-ca <tim@timdog.ca>
2024-10-17 15:05:24 -0600

    uopdates to code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,8 @@
-// NAVIGATION
+// Toggle Menu Bar
 document.addEventListener("scroll", showMenu, true);
 
 function showMenu() {
-  if (document.documentElement.scrollTop > 25) {
+  if (document.documentElement.scrollTop > 50) {
     document.getElementById("navigation").style.top = "0";
     document.getElementById("navigation").style.opacity = "1";
 
@@ -13,40 +13,47 @@ function showMenu() {
 }
 
 // Send Email
-
 function submitForm(form) {
   window.open("mailto:info@brownsquirreldesign.ca?subject=" + form.subject.value + "&body=Name:%20" + form.name.value + "%0D%0AMessage:%20" + form.message.value);
   return false; /* cancel submit or else page reloads */
 }
 
-// Squirrel Facts
 
-const rndInt = Math.floor(Math.random() * 6) + 1;
-
-const sqfacts = [
-  {"fact":"Squirrels can find food buried beneath a foot of snow."},
-  {"fact":"A squirrel's front teeth never stop growing."},
-  {"fact":"Squirrels may lose 25 percent of their buried food to thieves."},
-  {"fact":"They zigzag to escape predators."},
-  {"fact":"Squirrels may pretend to bury a nut to throw off potential thieves."},
-  {"fact":"A newborn squirrel is about an inch long."},
-  {"fact":"Humans introduced squirrels to most of our major city parks."},
-  {"fact":"Squirrels are acrobatic, intelligent, and adaptable."},
-  {"fact":"They get bulky to stay warm during the winter."},
-  {"fact":"Squirrels don't dig up all of their buried nuts, which results in more trees!"},
-  {"fact":"The Word 'Squirrel' Comes From Greek for 'Shadow Tail'"},
-  {"fact":"Only a Few Squirrels Hibernate"},
-  {"fact":"Some Ground Squirrels Make 'Rattlesnake Perfume'"},
-  {"fact":"There are 22 different species of squirrel found in Canada"}
-];
-
-document.getElementById("squirrelfacts").innerHTML = sqfacts[rndInt].fact;
+// Load Squirrel Facts & Set the P tag with random fact
+fetch('./assets/data/facts.json')
+  .then(response => response.json())
+  .then(data => {
+    const rndInt = Math.floor(Math.random() * data.length) + 1;
+    document.getElementById("squirrelfacts").innerHTML = data[rndInt].fact;
+  })
+  .catch(error => console.error('Error:', error));
 
 
+// set degree of linear-gradient for home section
 const rndDeg = Math.floor(Math.random() * 60) + 20;
-console.log(rndDeg);
-
-document.getElementById("home").setAttribute = ("background","linear-gradient(-" + rndDeg.fact + "deg, #c68b59, #fcdec0, #c68b59);");
-console.log('linear-gradient(-' + rndDeg + 'deg, #c68b59, #fcdec0, #c68b59);');
+document.getElementById("home").setAttribute = ("background","linear-gradient(-" + rndDeg + "deg, #c68b59, #fcdec0, #c68b59);");
 
 
+// hightlight active menu item
+const sections = document.querySelectorAll('section');
+const navli = document.querySelectorAll('header nav .main-navitems ul li');
+
+window.addEventListener("scroll", () => {
+
+  let current = '';
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navli.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
